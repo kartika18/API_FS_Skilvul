b846fb274066ae444f644d79767f670b34c06b12
kartika18 <skartika426@gmail.com>
2023-11-22 19:03:40 +0700

    added connection to database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const db_connection = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+db_connection
+  .then(() => {
+    console.log("Success connecting to mongoDB");
+  })
+  .catch(() => {
+    console.log("Failed connecting to mongoDB");
+  });
 
 app.listen(PORT, () => {
   console.log("Server running on PORT ", PORT);
